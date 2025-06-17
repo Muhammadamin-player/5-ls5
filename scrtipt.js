f1d0c6e7a02f9445c2243ff50834716fa92575b8
Muhammadamin-player <ibrohimovmuhammadamin888@gmail.com>
2025-06-17 12:04:46 +0500

    Add files via upload

---
+++ scrtipt.js
@@ -0,0 +1,13 @@
+let userName = prompt('ismingizni kiriting')
+let userAge = prompt('yoshingizni kiriting')
+let userSurname = prompt('familiyandiznni kiriting')
+let userFrom = prompt('shaxiringizni kiriting')
+
+let user = {
+    name: userName,
+    age: userAge,
+    surname: userSurname,
+    from: userFrom,
+}
+
+console.log(user)
